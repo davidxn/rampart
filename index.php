39d81c92f94e66f9673d757e4f295d13d9f9f4a0
David Newton <davidknewton@gmail.com>
2021-06-23 23:47:17 -0400

    Catching up

--- index.php
+++ index.php
@@ -10,13 +10,13 @@ require_once('./header.php');
 <div style="border: 1px solid gray; position: relative;">
 <center>
 <iframe id="ytplayer" type="text/html" width="640" height="360"
-  src="https://www.youtube.com/embed/RaCGb3QmbsM?autoplay=1&mute=1&controls=0&rel=0&loop=1&playlist=RaCGb3QmbsM&modestbranding=1&origin=http://ramp.teamouse.net"
+  src="https://www.youtube.com/embed/80NbTTyVLqU?autoplay=1&mute=1&controls=0&rel=0&loop=1&playlist=80NbTTyVLqU&modestbranding=1&origin=http://ramp.teamouse.net"
   frameborder="0"></iframe>
 </center>
 </div>
 <div id="vidmask">&nbsp;</div>
 </div>
-<p>In the end (aiming for somewhere around the start of July 2021), I'll release a compilation WAD containing all the levels wrapped up in a wider meta-game, and I'll be glad to provide any feedback you might want!</p>
+<p>After a playtesting period starting in July, I'll release a compilation WAD containing all the levels wrapped up in a wider meta-game, and I'll be glad to provide any feedback you might want!</p>
 
 <p>RAMP is hosted by <a href="http://twitter.com/DavidXNewton">DavidN or DavidXNewton</a>. Online, he is usually found in the form of a Scottish dwarf lop rabbit. Some people say he has a very small pet Doomguy.</p>
 <?
