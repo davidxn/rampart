89ef2d47e0cfbdb21c5866029f965a0f52a0d752
davidxn <davidknewton@gmail.com>
2021-07-07 20:46:43 -0400

    Undestroy

--- header.php
+++ header.php
@@ -6,11 +6,7 @@ require_once('_constants.php');
     <head>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="/upload.js?xcache=8" type="text/javascript"></script>
-<<<<<<< HEAD
         <link rel="stylesheet" href="/ramp.css?xcache=7">
-=======
-        <link rel="stylesheet" href="/ramp.css?xcache=5">
->>>>>>> 715c9beefc57e1cc21821b0324eb0bceaaad9f1b
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Exo">
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
@@ -27,16 +23,11 @@ require_once('_constants.php');
         <div class="outer">
             <div class="header">
                 <div class="titlebox"><a class="quickstart" href="/rampstarterpack.zip"><img src="/quickstartbutton.png"/></a></div>
-<<<<<<< HEAD
                 <div class="bannermessage">Fixing maps up and getting ready for open beta!</div>
-=======
-                <div class="bannermessage">Accepting map uploads until 6th July!</div>
->>>>>>> 715c9beefc57e1cc21821b0324eb0bceaaad9f1b
                 <div class="menubox">
                     <a href="/index.php"><div class="menuitem">Home</div></a>
                     <a href="/rules.php"><div class="menuitem">Rules</div></a>
                     <a href="/guide.php"><div class="menuitem">Getting Started</div></a>
-<<<<<<< HEAD
 <?php
 $update_verb = "Submit";
 if (ALLOW_NEW_UPLOADS == false) {
@@ -44,9 +35,6 @@ if (ALLOW_NEW_UPLOADS == false) {
 }
 ?>
                     <a href="/upload.php"><div class="menuitem"><?=$update_verb?> a Map</div></a>
-=======
-                    <a href="/upload.php"><div class="menuitem">Submit a Map</div></a>
->>>>>>> 715c9beefc57e1cc21821b0324eb0bceaaad9f1b
                     <a href="/download.php"><div class="menuitem">Download the Project</div></a>
                     <a target="_blank" href="https://discord.gg/afFGdGNhW2"><div class="menuitem">Discord</div></a>
                 </div>
