e75e7a96d8f709c431aee9de2fe829eaea30ac41
DavidN <davidknewton@gmail.com>
2022-06-02 02:40:19 +0000

    Change logic for PK3 updatedness, include fixed content folder

--- _functions.php
+++ _functions.php
@@ -2,3 +2,10 @@
 function get_source_wad_file_name($slot) {
     return ("RM" . (substr("0" . $slot, -2)) . ".WAD");
 }
+
+function get_mtime($file) {
+    if (is_file($file)) {
+        return filemtime($file);
+    }
+    return 0;
+}
--- handle_pk3_update.php
+++ handle_pk3_update.php
@@ -9,29 +9,50 @@ require_once($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "scripts/project_
 $redirect = isset($_GET['redirect']) ? $_GET['redirect'] : false;
 $nocache = isset($_GET['nocache']) ? $_GET['nocache'] : false;
 
-$lock_file_recent = file_exists(LOCK_FILE_COMPILE) && (time() - filemtime(LOCK_FILE_COMPILE)) < 600;
-$pk3_is_current = file_exists(get_project_full_path()) && (filemtime(CATALOG_FILE) < filemtime(get_project_full_path()));
+$lock_file_recent = file_exists(LOCK_FILE_COMPILE) && (time() - get_mtime(LOCK_FILE_COMPILE)) < 600;
+$pk3_is_current = true;
 
 if ($lock_file_recent) {
     echo json_encode(['success' => false, 'error' => 'Project is already being generated! Hold on a minute then try again']);
     die();
 }
-if (!$pk3_is_current) {
-    Logger::lg("Catalog is newer than latest build, so an update is required: " . (filemtime(CATALOG_FILE) - filemtime(get_project_full_path())));
-} else {
-    if (file_exists(get_project_full_path())) {
-        Logger::lg("Catalog is older than latest build - will serve existing one: " . (filemtime(CATALOG_FILE) - filemtime(get_project_full_path())));
-    } else {
-        Logger::lg("No snapshot exists - will create one");
+
+// Only perform PK3 check if we're not forcing a rebuild
+
+if ($nocache) {
+    Logger::lg("Nocache flag was passed to PK3 updater - will rebuild");
+    $pk3_is_current = false;
+}
+
+// Check for changes to the catalog file
+if (get_mtime(CATALOG_FILE) > get_mtime(get_project_full_path())) {
+    $pk3_is_current = false;
+    Logger::lg("Catalog file has newer data than latest snapshot - will rebuild");
+}
+
+// Now check the fixedcontent folder... if we don't have a script, just keep the answer from before
+if (!empty($GLOBALS["STATIC_CONTENT_MTIME_SCRIPT"])) {
+    $static_content_mtime = trim(shell_exec($GLOBALS["STATIC_CONTENT_MTIME_SCRIPT"]));
+    if (is_numeric($static_content_mtime) && !empty($static_content_mtime) && $static_content_mtime > get_mtime(get_project_full_path())) {
+        Logger::lg("Static content folder has newer data than latest snapshot - will rebuild");
+        $pk3_is_current = false;
     }
 }
 
-//If we already have a PK3 that's been generated after the last update of the catalogue file, terminate and instruct to serve the old one
-if (!$nocache && ($lock_file_recent || $pk3_is_current)) {
+// If we don't have a project file at all, we have to build
+if (!file_exists(get_project_full_path())) {
+    Logger::lg("No snapshot exists - will rebuild");
+    $pk3_is_current = false;
+}
+
+// If we've reached here and the pk3_is_current flag is still true, then we've decided the existing PK3 is still OK
+if ($pk3_is_current) {
+    Logger::lg("PK3 passed all checks - serving the existing one");
     echo json_encode(['success' => true, 'newpk3' => false]);
     die();
 }
 
+// Trigger a rebuild
 $handler = new Project_Compiler();
 if ($handler->compile_project()) {
     if ($redirect) {
