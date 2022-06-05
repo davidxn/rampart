1499b44e405a4d2c81b29ee655b25e2773621317
DavidN <davidknewton@gmail.com>
2022-06-05 19:57:58 +0000

    Add SNDINFO handling, hub map and ZScript version options

---
+++ scripts/sndinfo_handler.php
@@ -0,0 +1,28 @@
+<?php
+require_once("_constants.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "scripts/logger.php");
+
+class Sndinfo_Handler {
+    
+    private $current_line_number = 0;
+    private $input_lines = [];
+    private $requested_sound_lump_names = [];
+    
+    public function __construct($bytes) {
+        $this->input_lines = split(PHP_EOL, $bytes);
+    }
+    
+    public function parse() {
+        while ($this->current_line_number < count($this->input_lines)) {
+            $current_line = $this->input_lines[$this->current_line_number];
+            $tokens = preg_split('/\s+/', trim($current_line));
+            if (count($tokens) == 2 && substr($tokens[0], 0, 1) != "$") {
+                //We're going to assume this is a sound name followed by a lump name. Add the lump name to our requested sounds.
+                $this->requested_sound_lump_names[] = strtoupper($tokens[1]);
+                Logger::pg("Added " . strtoupper($tokens[1]) . " to requested sound lump names");
+            }
+            $this->current_line_number++;
+        }
+        return [$this->input_lines, $this->requested_sound_lump_names];
+    }
+}
