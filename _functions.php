c10d53a0f14838646c857d5ef437f5a574861f7c
DavidN <davidknewton@gmail.com>
2022-07-04 04:24:22 +0000

    Correct support for maps above 99

--- _functions.php
+++ _functions.php
@@ -1,6 +1,10 @@
 <?php
 function get_source_wad_file_name($slot) {
-    return ("RM" . (substr("0" . $slot, -2)) . ".WAD");
+    // Prefix under 10 with a 0
+    if ($slot >= 0 && $slot < 10) {
+        return ("RM" . (substr("0" . $slot, -2)) . ".WAD");
+    }
+    return "RM" . $slot . ".WAD";
 }
 
 function get_mtime($file) {
